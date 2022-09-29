8c40c5ea30f7175474f5e35b136855d6f802ce3d
David Neu <mail@david-neu.de>
2022-09-29 14:05:45 +0200

    Fixed Up / Down Events on some Keys

--- LogiRemoteRemap.cpp
+++ LogiRemoteRemap.cpp
@@ -12,7 +12,11 @@ enum ScanCode
     SCANCODE_N = 49,
     SCANCODE_ESC = 0x01,
     SCANCODE_PGDOWN = 81,
-    SCANCODE_PGUP = 73
+    SCANCODE_PGUP = 73,
+    SCANCODE_1 = 2,
+    SCANCODE_2 = 3,
+    SCANCODE_3 = 4,
+    SCANCODE_4 = 5
 };
 
 int main()
@@ -34,19 +38,43 @@ int main()
     {
         size_t length = interception_get_hardware_id(context, device, hardware_id, sizeof(hardware_id));
 
-        int res = wcscmp(hardware_id, hardware_match_id);
+       int res = wcscmp(hardware_id, hardware_match_id);
 
        if (res == 0) {
-            if(stroke.code == SCANCODE_PLAY || stroke.code == SCANCODE_ESC) stroke.code = SCANCODE_A;
-            if(stroke.code == SCANCODE_POINT) stroke.code = SCANCODE_B;
+           if (stroke.code == SCANCODE_PLAY || stroke.code == SCANCODE_ESC) {
+               stroke.code = SCANCODE_3;
+               std::cout << "PLAY / ESC ";
+           }
+           if (stroke.code == SCANCODE_POINT) {
+               stroke.code = SCANCODE_4;
+               std::cout << "POINT";
+           }
 
-            if (stroke.code == SCANCODE_PGUP) stroke.code = SCANCODE_P;
-            if (stroke.code == SCANCODE_PGDOWN) stroke.code = SCANCODE_N;
+           if (stroke.code == SCANCODE_PGUP) {
+               stroke.code = SCANCODE_1;
+               std::cout << "PGUP";
+           }
+           if (stroke.code == SCANCODE_PGDOWN) {
+               stroke.code = SCANCODE_2;
+               std::cout << "PGDOWN";
+           }
 
             if (stroke.state == INTERCEPTION_KEY_E0) stroke.state = INTERCEPTION_KEY_DOWN;
             if (stroke.state == INTERCEPTION_KEY_E1) stroke.state = INTERCEPTION_KEY_UP;
+            if (stroke.state == 3) stroke.state = INTERCEPTION_KEY_UP;
+
+            std::cout << stroke.state;
+
+            if (stroke.state == INTERCEPTION_KEY_DOWN) {
+                std::cout << " - D\n";
+            }
+            if (stroke.state == INTERCEPTION_KEY_UP) {
+                std::cout << " - U\n";
+            }
 
             interception_send(context, device, (InterceptionStroke*)&stroke, 1);
+
+            //interception_send(context, device, (InterceptionStroke*)&stroke, 1);
             continue;
         }
         else {
